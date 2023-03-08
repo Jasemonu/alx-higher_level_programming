ab30895cc40edbf03732c1a3c9b42e3dfd9c4846
Jasemonu <jasemonu@gmail.com>
2023-03-07 21:42:32 -0800

    8-concat_edges.py

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(f"{str[39:66]}", end=" ")
+print(f"{str[114:118]}", end=" ")
+print(f"{str[0:6]}")
