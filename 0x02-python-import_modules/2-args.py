9fb51afe115cda4eb9abee58dddfeacac2f6c16c
Jasemonu <jasemonu@gmail.com>
2023-03-13 17:06:26 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,11 @@ if __name__ == "__main__":
 
     from sys import argv
 argv_length = len(argv)-1
-print("{} arguments:" .format(argv_length))
+if argv_length == 0:
+    print("0 arguments.")
+elif argv_length == 1:
+    print("1 argument.")
+else:
+    print("{} arguments:" .format(argv_length))
 for i in range(argv_length):
     print("{} : {}".format(i + 1, argv[i+1]))
