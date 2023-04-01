4999f84a93b77a6e0afd0406a588d5f62a9dccd1
Jasemonu <jasemonu@gmail.com>
2023-04-01 04:17:59 -0700

    102-magic_calculation.py

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+                result = result + (a ** b) ** (a ** -1)
+        except:
+            result = result + a + b
+        break
+    else:
+        return result
