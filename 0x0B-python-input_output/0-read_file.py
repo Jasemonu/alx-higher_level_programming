cb0639ea54575df3f3a36315d11bbc885f3e0279
Jasemonu <jasemonu@gmail.com>
2023-04-12 13:15:16 -0700

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" A function that reads a file and prints to the stdout"""
+
+
+def read_file(filename=""):
+
+    """Arg: filename: Name of the file to be read from"""
+
+    with open(filename, "r", encoding="utf-8") as fl:
+        text = fl.read()
+    print(text)
