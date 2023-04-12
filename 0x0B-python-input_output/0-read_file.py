113a2d218f5cd274cbdb5acbb94eea2123b3f042
Jasemonu <jasemonu@gmail.com>
2023-04-12 13:40:59 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
 
     """Reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, "r", encoding="utf-8") as fl:
-        print(fl.read(), end="")
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
