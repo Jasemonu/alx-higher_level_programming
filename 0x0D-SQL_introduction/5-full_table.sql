b84cace69830d21568d86b8614aa9f3c02416c50
Jasemonu <jasemonu@gmail.com>
2023-05-17 06:36:48 -0700

    5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -3,4 +3,4 @@
 -- The database name will be passed as an argument of the mysql command
 -- The database name will be passed as an argument of the mysql command
 
-SHOW CREATE TABLE hbtn_0c_0.first_table
+SHOW CREATE TABLE first_table
