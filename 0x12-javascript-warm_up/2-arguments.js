a868d144826788118e120f405f8d5f21d4866ae5
Jasemonu <jasemonu@gmail.com>
2023-06-12 13:38:31 -0700

    pushing three files

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const count = args.length;
+f (count === 2) {
+  console.log('No argument');
+} else if (count === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
