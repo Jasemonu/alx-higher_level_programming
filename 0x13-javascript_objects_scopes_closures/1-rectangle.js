201317c49b3a759cb423eb806492731be42b3732
Jasemonu <jasemonu@gmail.com>
2023-06-14 16:24:02 -0700

    1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/**
+* A calss of a Rectangle with a constructor
+*/
+class Rectangle {
+	constructor(w, h) {
+		this.width = w
+		this.height = h
+	}
+}
+
+module.exports = Rectangle;
