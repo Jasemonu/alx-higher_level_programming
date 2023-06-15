4bbf436dc8d185a8dd75f753b1e68c8057665dbf
Jasemonu <jasemonu@gmail.com>
2023-06-14 17:49:59 -0700

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+/**
+ * Check the parameters provided
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let myChar = '';
+      for (let j = 0; j < this.width; j++) {
+        myChar += 'X';
+      }
+
+      console.log(myChar);
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
