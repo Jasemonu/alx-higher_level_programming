4d45bdcdb54940a3fb373b9a8e3a7d8eb40d7220
Jasemonu <jasemonu@gmail.com>
2023-06-17 17:56:49 -0700

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,7 +14,9 @@ def list_cities():
         db=sys.argv[3]
         )
     db_cursor = db_connect.cursor()
-    db_cursor.execute("SELECT * FROM cities ORDER BY id ASC")
+    db_cursor.execute("SELECT cities.id, cities.name, states.name \
+            FROM cities JOIN states ON cities.state_id \
+            = states.id ORDER BY cities.id ASC")
     cities = db_cursor.fetchall()
     for city in cities:
         print(city)
