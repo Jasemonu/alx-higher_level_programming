b72dac6f03b1ff10a89f8117ef26acc355ded3f5
Jasemonu <jasemonu@gmail.com>
2023-06-18 10:15:30 -0700

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,7 +11,7 @@ from model_state import Base, State
 
 if __name__ == '__main__':
 
-db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         argv[1], argv[2], argv[3])
 
     engine = create_engine(db_url)
