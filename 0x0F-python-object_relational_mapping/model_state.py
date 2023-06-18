cb6dd0b09789e34ac1e4747f230c41b5aff4764f
Jasemonu <jasemonu@gmail.com>
2023-06-17 19:02:07 -0700

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 """
-A python file that contains the class definition of a
-State and an instance Base = declarative_base():
+This script defines a State class and
+a Base class to work with MySQLAlchemy ORM.
 """
+from sys import argv
+from model_state import Base, State
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (create_engine)
 
-Base = declarative_base()
-
-
-class State(Base):
-    __tablename__ = 'states'
-
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(128), nullable=False)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
