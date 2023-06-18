51d35af05a4e03744dcab93556bf5f518404d8c2
Jasemonu <jasemonu@gmail.com>
2023-06-18 16:44:13 -0700

    11-model_state_insert.py

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+A script that adds the State object
+“Louisiana” to the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    """
+    Access to the database and get a state
+    from the database.
+    """
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            username, password, database))
+    Base.metadata.bind = engine
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    louisiana = State(name="Louisiana")
+    session.add(louisiana)
+    session.commit()
+    print(louisiana.id)
+    session.close()
