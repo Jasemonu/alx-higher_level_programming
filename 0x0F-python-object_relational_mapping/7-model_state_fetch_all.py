7e3d05f6cfa18265db130aa6c63ba11cc8af9478
Jasemonu <jasemonu@gmail.com>
2023-06-18 10:08:51 -0700

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,33 +4,20 @@ A script that lists all State objects
 from the database hbtn_0e_6_usa
 """
 
-import sys
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == '__main__':
 
-    """ Get the arguments """
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
 
-    """ Create the engine and establish the database connection """
-    db_link = "f'mysql+mysqldb://{username}: \
-        {password}@localhost:3306/{database}'"
-    engine = create_engine(db_link)
-    Base.metadata.bind = engine
-
-    """ Create a session to interact with the database """
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
+
     session = Session()
 
-    """ Query all State objects and sort them by id in ascending order """
-    states = session.query(State).order_by(State.id).all()
-
-    """ Display the results """
-    for state in states:
-        print(f'{state.id}: {state.name}')
-
-    session.close()
+    for instance in session.query(State).order_by(State.id):
+        print('{0}: {1}'.format(instance.id, instance.name))
