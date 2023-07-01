abbb53d3fe0f66a926ed745e46d4ee54d1616ece
Jasemonu <jasemonu@gmail.com>
2023-07-01 06:18:24 -0700

    6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+A Python script that takes in a URL and an email address
+sends a POST request to
+the passed URL with the email as a parameter, and finally displays the body of the response.
+"""
+
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+
+    value = {"email": email}
+    body = requests.post(url, data=value)
+    print(body.text)
